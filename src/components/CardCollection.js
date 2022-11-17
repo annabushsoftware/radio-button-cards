9b7503dc9bfe8e710c322770ac3a69ea6b638c87
Anna Bush <annabushsoftware@gmail.com>
2022-11-17 14:57:38 -0500

    make cards list dynamic

--- src/components/CardCollection.js
+++ src/components/CardCollection.js
@@ -2,17 +2,32 @@ import '../styles/card-collection.scss';
 import Card from './Card';
 
 function CardCollection() {
+    var cardCollection = getCards();
+    var cards = createCards(cardCollection);
+
     return (
         <div className="card-collection">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {cards}
         </div>
     )
+}
+
+function getCards() {
+    // TODO - fetch cards from db
+    var cardCollection = [];
+
+    return cardCollection;
+}
+
+function createCards(cardCollection) {
+    var cards = [];
+
+    for(var i = 0; i < 10; i++) {
+        const key = `card-${i}`;
+        cards.push(<Card key={key} />);
+    }
+
+    return cards;
 }
 
 export default CardCollection
