19620d9c260c694c320b98643a54379bfa1eb1d2
Anna Bush <annabushsoftware@gmail.com>
2022-11-17 17:05:07 -0500

    big refactoring

--- src/components/CardCollection.js
+++ src/components/CardCollection.js
@@ -1,54 +1,67 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useCallback} from 'react';
 import '../styles/card-collection.scss';
+import collectionJson from '../cardsCollection.json';
 import Card from './Card';
 
 function CardCollection() {
-    const [cards, setCards] = useState([]);
-    const [selectedCard, setSelectedCard] = useState(null);
+    // state
+    const [initialLoad, setInitialLoad] = useState(true);
+    const [cardsData, setCardsData] = useState([]);
 
+    // callbacks
+    const populateCards = useCallback(() => {
+        console.log('in callback', initialLoad)
+        if(initialLoad || cardsData.length == 0) {
+            console.log('fetching cards');
+            // TODO - fetch cards from db
+            setTimeout(() => {
+                setInitialLoad(false);
+
+                var stringified = JSON.stringify(collectionJson)
+                var cardCollection = JSON.parse(stringified);
+
+                cardCollection.cards.forEach((card) => {
+                    card.selected = false;
+                })
+                setCardsData(cardCollection.cards);
+            }, 3 * 1000);
+        }
+    }, [initialLoad, cardsData, setCardsData])
+
+    function cardClicked(key) {
+        console.log('card clicked ', key);
+
+//        setSelectedCard(key);
+    }
+
+    // effects
+    useEffect(() => {
+        populateCards();
+    }, [populateCards]);
+
+    // render
     console.log('rendering')
 
-    useEffect(() => {
-        if(cards.length == 0) {
-            console.log('getting cards')
-            populateCards();
-        }
-    });
+    const cards = () => {
+        var cards = [];
+
+        cardsData.forEach((cardData) => {
+            cards.push(
+                <Card
+                    key={'card-' + cardData.id}
+                    selected={cardData.selected}
+                    cardClicked={() => cardClicked(cardData.id)}
+                />);
+        })
+
+        return cards;
+    }
 
     return (
         <div className="card-collection">
-            {cards}
+            {cards()}
         </div>
     )
-
-    function populateCards() {
-        // TODO - fetch cards from db
-        setTimeout(() => {
-            var cardCollection = [];
-            createCards(cardCollection);
-        }, 3 * 1000);
-    }
-
-    function createCards(cardCollection) {
-        var cards = [];
-
-        for(var i = 0; i < 10; i++) {
-            const key = `card-${i}`;
-            cards.push(
-                <Card
-                    key={key}
-                    cardClicked={() => cardClicked(key)}
-                />);
-        }
-
-        setCards(cards);
-        setSelectedCard(null);
-    }
-
-    function cardClicked(key) {
-        console.log('card clicked ', key)
-        setSelectedCard(key);
-    }
 }
 
 export default CardCollection
